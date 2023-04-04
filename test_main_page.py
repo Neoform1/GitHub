accede343d3e2c90477ea60d0a6d8c6dbf6a9d34
Artyom <tyoma24@gmail.com>
2023-04-04 12:37:49 +0300

    edits step 4.2 with locators

--- test_main_page.py
+++ test_main_page.py
@@ -2,11 +2,18 @@ from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 import time
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
 
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     time.sleep(2)
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     time.sleep(2)
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
