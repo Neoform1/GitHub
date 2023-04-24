7ce7021a54ab39db3daa99316a94554218e60547
Artyom <tyoma24@gmail.com>
2023-04-24 13:47:36 +0300

    # 'negative check'

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -13,7 +13,7 @@ class BasketPage(BasePage):
         assert is_element_present, "YOUR BASKET_IS_not_EMPTY"
 
         # is_element_not_present = self.is_not_element_present(*MainPageLocators.EMPTY_BASKET)
-        # assert is_element_not_present, "'negative check' YOUR BASKET_IS_EMPTY"
+        # assert is_element_not_present, "'negative check' YOUR BASKET_IS_EMPTY"        # 'negative check'
 
     def basket_empty_text_present(self):
 
@@ -21,4 +21,4 @@ class BasketPage(BasePage):
         assert is_present, "'Empty' text is not present"
 
         # is_not_present = self.is_not_element_present(*MainPageLocators.YOUR_BASKET_IS_EMPTY)
-        # assert is_not_present, "'negative check' -Empty- text is present"
+        # assert is_not_present, "'negative check' -Empty- text is present"         # 'negative check'
